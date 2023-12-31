fd9f6cc8799524a905c0ef7aec83b00ec79ea1d2
Sripadwallabha D. Kollur <sripadkollur@gmail.com>
2023-12-31 21:28:02 +0530

    Completed Hello, World lesson

--- hello.go
+++ hello.go
@@ -2,15 +2,38 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	hindi              = "हिंदी"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	hindiHelloPrefix   = "नमस्ते, "
+)
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case hindi:
+		prefix = hindiHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,17 +4,34 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
-		want := "+Hello, World"
+		got := Hello("", "")
+		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Hindi", func(t *testing.T) {
+		got := Hello("श्रीपाद वल्लभ", "हिंदी")
+		want := "नमस्ते, श्रीपाद वल्लभ"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
